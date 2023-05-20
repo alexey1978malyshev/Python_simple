d1c68c81a7047bb5542a58b6133313bed5cd1495
A9219 <alexey1978malyshev@gmail.com>
2023-05-20 15:48:07 +0300

    comments added

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import redis
 
 with redis.Redis() as redis_client:
-    value = redis_client.brpop("queue")
+    value = redis_client.brpop("queue") #brpop - клиент ждет, пока в очереди не появятся задачи(если там их нет) - возвращает кортеж из названия очереди и значений
 
-print(int(value[1]))
+print(int(value[1])) # обращаемся по индексу, т.к. первое значение кортежа - название очереди
