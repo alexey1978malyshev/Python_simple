0931bba7ffdcfd061e2e02f90ee829f93db8dbc7
A9219 <alexey1978malyshev@gmail.com>
2024-07-21 16:52:51 +0300

    'comm_added'

--- Seminar8/hash_calc.py
+++ Seminar8/hash_calc.py
@@ -4,15 +4,15 @@ import io, hashlib
 
 
 
-
-def export_hash(in_file_name: str, out_file_name: str):
-    with open(in_file_name, "rb") as f:
-        digest = hashlib.file_digest(f, "sha256")
-    hash_sum = digest.hexdigest()
+#функция для вычисления хэша файла:
+def export_hash(in_file_name: str, out_file_name: str): # аргументами ф-ции приходят адреса исходного файла и файла с хэш-суммой , куда будем сохранять хэш
+    with open(in_file_name, "rb") as f: # открываем исх.файл на чтение в бинарном режиме
+        digest = hashlib.file_digest(f, "sha256") #возвращаем объект дайджеста
+    hash_sum = digest.hexdigest()#возвращаем строкосый хэш
     MAIN_DIR = abspath(dirname(__file__))
     full_name = join(MAIN_DIR, out_file_name + '.txt')
     with open(full_name, mode='a', encoding='utf-8') as file:
-        file.write(f"Файл: {in_file_name} *** Хэш: {hash_sum}\n")
+        file.write(f"Файл: {in_file_name} *** Хэш: {hash_sum}\n") #записываем хэш в файл
 
 
 export_hash("phones11.csv","sha256_sum")
